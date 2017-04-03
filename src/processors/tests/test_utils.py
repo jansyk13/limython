da8efe3fcd1b0edfee0a58f670fda679e203a7c0
jansykora <jan.sykora@gooddata.com>
2017-04-03 20:16:39 +0200

    test

--- src/processors/tests/test_utils.py
+++ src/processors/tests/test_utils.py
@@ -1,10 +1,10 @@
-
-import numpy.testing as npt
 import logging as log
+import numpy as np
 import pandas as pd
 import sys
 import unittest
-import transformation.url as url
+import processors.utils as utils
+import processors.dummy_processor as processor
 
 log.basicConfig(stream=sys.stdout, level=log.DEBUG,
                 format='%(asctime)-15s %(threadName)s %(filename)s %(levelname)s %(message)s')
@@ -13,4 +13,10 @@ log.basicConfig(stream=sys.stdout, level=log.DEBUG,
 class TestUtils(unittest.TestCase):
 
     def testParse(self):
-        
+        df = pd.DataFrame({"payload_size": [10, 20, 30, 40, 50]})
+        p = np.array([10, 20, 30, 40, 50])
+        rmse, rsquarred = utils.process_and_compute_stats(
+            processor.DummyProcessor(), df, p)
+
+        self.assertEqual(rmse, 0)
+        self.assertEqual(rsquarred, 1.0)
