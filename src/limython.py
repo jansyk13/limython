2d51dcdc0bc84e1d0b4c2fccc2d03df67e30777a
jansykora <jan.sykora@gooddata.com>
2017-04-03 17:55:45 +0200

    blas setup

--- src/limython.py
+++ src/limython.py
@@ -16,7 +16,7 @@ import processors.utils as processor_utils
 import sys
 import transformation.categorical as categorical
 import validation.kfold as kfold
-import blas as blas
+import blas.utils as blas
 
 log.basicConfig(stream=sys.stdout, level=log.INFO,
                 format='%(asctime)-15s %(threadName)s %(filename)s %(levelname)s %(message)s')
@@ -149,7 +149,7 @@ def main_wrapper():
 
 def main():
     try:
-        with blas.utils.num_threads(32):
+        with blas.num_threads(32):
             main_wrapper()
     except Exception:
         log.info('action=end-with-exception')
