7b93c42423204ff50e800ec85b9258338ba96483
jansykora <jan.sykora@gooddata.com>
2017-04-04 22:42:18 +0200

    shell ols

--- ridge.sh
+++ ridge.sh
@@ -7,16 +7,16 @@ declare -a arr=("-a=normalize=False,fit_intercept=False"
                 "-a=normalize=True,fit_intercept=False -u=true"
                 "-a=normalize=False,fit_intercept=True -u=true"
                 "-a=normalize=True,fit_intercept=True -u=true"
-                "-a=normalize=False,fit_intercept=False -u=true -f=method,url,payload_size"
-                "-a=normalize=True,fit_intercept=False -u=true -f=method,url,payload_size"
-                "-a=normalize=False,fit_intercept=True -u=true -f=method,url,payload_size"
-                "-a=normalize=True,fit_intercept=True -u=true -f=method,url,payload_size"
+                "-a=normalize=False,fit_intercept=False -u=true -f=method,url"
+                "-a=normalize=True,fit_intercept=False -u=true -f=method,url"
+                "-a=normalize=False,fit_intercept=True -u=true -f=method,url"
+                "-a=normalize=True,fit_intercept=True -u=true -f=method,url"
                 )
 
 mkdir -p "results"
 for i in "${arr[@]}"
 do
    echo "$i"
-   touch "results/ols_$i"
-   python src/limython.py -n=3 -l=10000 -m=ols -r=false -p=best-counter $i > "results/ols_$i" &
+   touch "results/ridge_$i"
+   python src/limython.py -n=3 -l=100 -m=ridge -r=false -p=best-counter $i > "results/ridge_$i" &
 done
