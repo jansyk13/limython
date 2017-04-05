7c1b77703154b001c7f8bfae8ace9d757b8cde76
jansykora <jan.sykora@gooddata.com>
2017-04-05 11:29:40 +0200

    parsing + sgd shell

---
+++ result_to_csv.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import sys
+import re
+from os import listdir
+from os.path import isfile, join
+
+to_extract = sys.argv[1]
+path = sys.argv[2]
+files = [f for f in listdir(path) if isfile(join(path, f))]
+for _file in files:
+    with open(path + _file, "r") as f:
+        result = []
+        name = _file.replace("ols_", "").replace("ridge_", "").replace(
+            "tree_", "").replace("lasso_", "").replace("sgd_", "")
+        result.append(name)
+        stdev = None
+        rmse = None
+        rsquarred = None
+        for line in f:
+            if (to_extract == u'stdev'):
+                if ("MainThread utils.py INFO action=utilization value=" in line):
+                    r = re.search("^.*MainThread.*\[(.*)\].*$", line)
+                    result.append("=STDEV(%s)" % r.group(1))
+                    continue
+            if (to_extract == u'rsquarred'):
+                if ("MainThread kfold.py INFO action=kfold" in line):
+                    r = re.search(
+                        "^.*MainThread.*rmse=(.*) rsquarred=(.*).*$", line)
+                    result.append("%s" % r.group(2))
+                    continue
+            if (to_extract == u'timer'):
+                if ("MainThread timer.py INFO action=timer" in line):
+                    r = re.search(
+                        "^.*MainThread.*value=(.*).*$", line)
+                    if (float(r.group(1)) > 1):
+                        result.append("%s" % r.group(1))
+                    continue
+            if (to_extract == u'rmse'):
+                if ("MainThread kfold.py INFO action=kfold" in line):
+                    r = re.search(
+                        "^.*MainThread.*rmse=(.*) rsquarred=(.*).*$", line)
+                    result.append("%s" % r.group(1))
+                    continue
+
+        print(";".join(result))
